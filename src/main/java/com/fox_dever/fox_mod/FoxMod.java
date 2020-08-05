dddc9273bff0a83ec3f231826784eefe0a34dec8
Fox <developed.by.fox@gmail.com>
2020-08-05 19:41:23 +0300

    sceleton

---
+++ src/main/java/com/fox_dever/fox_mod/FoxMod.java
@@ -0,0 +1,32 @@
+package com.fox_dever.fox_mod;
+
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("fox_mod")
+public class FoxMod
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+
+    public FoxMod()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) { }
+
+    private void doClientStuff(final FMLClientSetupEvent event) { }
+
+}
